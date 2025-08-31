1ee2e361016c4ae97e2bb0e18b7ff1e981b1b76b
MD. SAQIB <87118773+Saqib-develops@users.noreply.github.com>
2025-09-01 01:51:51 +0530

    Update translator_middleware.py

--- translator_middleware.py
+++ translator_middleware.py
@@ -5,18 +5,14 @@ import asyncio
 
 translator = Translator()
 
-async def preprocess_user_message(user_text):
-    detected = await translator.detect(user_text)
+def preprocess_user_message(user_text: str):
+    detected = translator.detect(user_text)
     detected_lang = detected.lang if detected else "en"
-
-    # Await the coroutine
-    translated = await translator.translate(user_text, src=detected_lang, dest="en")
+    translated = translator.translate(user_text, src=detected_lang, dest="en")
     return translated.text, detected_lang
 
-
-def postprocess_bot_response(bot_text, target_lang):
-    """Translate Rasa response (English) back to user's language"""
-    if target_lang != "en":
-        translated = translator.translate(bot_text, src="en", dest=target_lang).text
-        return translated
-    return bot_text
+def postprocess_bot_response(bot_text: str, target_lang: str):
+    if target_lang == "en":
+        return bot_text
+    translated = translator.translate(bot_text, src="en", dest=target_lang)
+    return translated.text
